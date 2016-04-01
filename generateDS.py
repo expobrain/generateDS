41f199b2dfa196f1e7e243541bc0265d1022346a
Dave Kuhlman <dkuhlman@davekuhlman.org>
2016-04-01 11:02:07 -0700

    New version 2.21a after Aleksandr's fixes to GUI front end

--- generateDS.py
+++ generateDS.py
@@ -202,7 +202,7 @@ logging.disable(logging.INFO)
 # Do not modify the following VERSION comments.
 # Used by updateversion.py.
 ##VERSION##
-VERSION = '2.20b'
+VERSION = '2.21a'
 ##VERSION##
 
 if sys.version_info.major == 2:
