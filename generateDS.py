722c0edfbd71bef85f263fef2664ddb8363cb6e6
Dave Kuhlman <dkuhlman@davekuhlman.org>
2016-09-14 11:27:07 -0700

    Updated version number

--- generateDS.py
+++ generateDS.py
@@ -204,7 +204,7 @@ logging.disable(logging.INFO)
 # Do not modify the following VERSION comments.
 # Used by updateversion.py.
 ##VERSION##
-VERSION = '2.22b'
+VERSION = '2.23a'
 ##VERSION##
 
 if sys.version_info.major == 2:
